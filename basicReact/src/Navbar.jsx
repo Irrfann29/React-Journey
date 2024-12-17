4d8a4689ba279f48dcedd510a223949b98f3e8cc
Irrfann29 <mrirfanhashmi2303@gmail.com>
2024-12-17 18:19:26 +0530

    Minor Changes

---
+++ basicReact/src/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+            <nav className='navBar'>
+                <ul>
+                    <li className='li'>Home</li>
+                    <li className='li'>About</li>
+                    <li className='li'>Contact</li>
+                    
+                </ul>
+            </nav>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
